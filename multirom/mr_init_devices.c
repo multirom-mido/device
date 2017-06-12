ecfb31d3b3aa11a23c3a2cbb8e9457a6434f9590
Sai Vishal <thug@droidthug.me>
2017-06-12 12:15:17 +0530

    multirom: correct block devices in init

--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -15,12 +15,12 @@ const char *mr_init_devices[] =
     "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0",
     "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001",
     "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p37", // boot
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p52", // cache
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p19", // modem
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p30", // persist
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p53", // system
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p54", // userdata
+    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p21", // boot
+    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p25", // cache
+    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p1", // modem
+    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p26", // persist
+    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p24", // system
+    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p49", // userdata
     "/sys/bus/mmc",
     "/sys/bus/mmc/drivers/mmcblk",
     "/sys/module/mmc_core",
@@ -29,7 +29,7 @@ const char *mr_init_devices[] =
     "/sys/devices/soc.0/gpio_keys.70/input*",
     "/sys/devices/virtual/input*",
     "/sys/devices/virtual/misc/uinput",
-    "/sys/devices/soc/78b7000.i2c/i2c-3/3-0020/input*",
+    "/sys/devices/soc/78b7000.i2c/i2c-3/3-0038/input*",
 
     // for adb
     "/sys/devices/virtual/tty/ptmx",
